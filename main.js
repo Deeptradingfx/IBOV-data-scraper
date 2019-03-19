02494a486a2a22a4e7b0aab59ee78fd4af9c35b4
Elastic Cloud User <dc-user@ech-10-157-148-191.mastercard.int>
2019-03-19 20:55:22 +0300

    Gathering data

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 const jsonfile = require('jsonfile');
 var fs = require('fs');
+const cron = require("node-cron");
 
 const cookiesFilePath = "./cookies.json"
-    
+
 const saveCookies = async (page) => {
 
     // Save Session Cookies
@@ -11,16 +12,16 @@ const saveCookies = async (page) => {
 
     // Write cookies to temp file to be used in other profile pages
     jsonfile.writeFile(cookiesFilePath, cookiesObject, { spaces: 2 },
-    
-    function(err) { 
-        if (err) {
-        console.log('The file could not be written.', err)
-        }
-    
-        console.log('Session has been successfully saved')
-    })
 
-} 
+        function (err) {
+            if (err) {
+                console.log('The file could not be written.', err)
+            }
+
+            console.log('Session has been successfully saved')
+        })
+
+}
 
 const openSession = async (page) => {
 
@@ -28,58 +29,105 @@ const openSession = async (page) => {
     const cookiesArr = require(`${cookiesFilePath}`)
     if (cookiesArr.length !== 0) {
         for (let cookie of cookiesArr) {
-        await page.setCookie(cookie)
+            await page.setCookie(cookie)
         }
         console.log('Session has been loaded in the browser')
         return true
     }
 
-} 
+}
 
 const login = async (page) => {
-    
-        await page.setViewport({width: 1200, height: 720})
-        await page.goto('https://br.tradingview.com/#signin', { waitUntil: 'networkidle0' }); // wait until page load]
-    
-        // click and wait for navigation
-        // await page.click('.tv-header__link--signin'),
-        // await page.waitForNavigation({ waitUntil: 'networkidle0' })
-    
-        await page.type('[type=text]', "");
-        await page.type('[type=password]', "");
 
-        try {
+    await page.setViewport({ width: 1200, height: 720 })
+    await page.goto('https://br.tradingview.com/#signin', { waitUntil: 'networkidle0' }); // wait until page load]
+
+    const username = process.env.TRADING_VIEW_USERNAME
+    const password = process.env.TRADING_VIEW_PASSWORD
+
+    await page.type('[type=text]', username);
+    await page.type('[type=password]', password);
+
+    try {
+
+        const [response] = await Promise.all([
+            page.waitForNavigation({ timeout: 6, waitUntil: 'networkidle0' }), // The promise resolves after navigation has finished
+            page.click('[type=submit]'), // Clicking the link will indirectly cause a navigation
+        ]);
+
+    } catch (error) {
+        console.log('catch')
+    }
+}
+
+function getCurrentTime() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+    console.log(hour, min, sec)
+}
+
+
+const getData = async function (page) {
+    const {
+        setIntervalAsync,
+        clearIntervalAsync
+    } = require('set-interval-async/dynamic')
+
+    setIntervalAsync(
+        async () => {
             
-            const [response] = await Promise.all([
-                page.waitForNavigation({timeout: 6, waitUntil:'networkidle0'}), // The promise resolves after navigation has finished
-                page.click('[type=submit]'), // Clicking the link will indirectly cause a navigation
-            ]);
+            const hour = await page.$eval('.inner-2FptJsfC-', el => el.innerText)
+            const price = await page.$eval('.dl-header-price', el => el.innerText)
+            console.log(`Hour: ${hour} - Price: ${price}`)
 
-        } catch (error) {
-            console.log('catch')
-        }
-}   
+        },
+        1000
+    )
+
+}
 
 // Start function
-const start = async function() {
+const start = async function () {
 
-    const browser = await puppeteer.launch({headless:false});
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], userDataDir: "./user_data" });
+
     const page = await browser.newPage()
 
     const previousSession = fs.existsSync(cookiesFilePath)
 
     try {
-        await login(page);        
+        await login(page);
     } catch (error) {
-        console.log('Done login')
     }
 
-    console.log('saving cookies')
-    await saveCookies(page)
 
-    await page.waitFor(3000);
-    await page.goto('https://br.tradingview.com/chart/fu7bcOd1/', { waitUntil: 'networkidle0' }); // wait until page load]
+    try {
 
+        const errorMesssage = await page.$eval('.tv-http-error-page__title', el => el.innerText)
+        if (errorMesssage) {
+            console.log("Error in login")
+            return false;
+        }
+
+    } catch (error) {
+        console.log('Login successfully')
+    }
+
+    await page.waitFor(1000);
+    await page.goto('https://www.tradingview.com/chart/fu7bcOd1/', { waitUntil: 'networkidle0' }); // wait until page load]
+
+    const name = await page.$eval('.dl-header-price', el => el.innerText)
+    console.log(name)
+
+    getData(page)
 }
 
 start()
